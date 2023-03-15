a202e45e70a5b32b331c686037463a4a1472b3cf
Juan Pablo Galeotti <jgaleotti@gmail.com>
2023-03-15 15:04:44 -0300

    fuzz targets for integration tests of integer-overflow-or-underflow

--- cargo-fuzz/integer-overflow-or-underflow/fuzz/fuzz_targets/fuzz_target_1.rs
+++ cargo-fuzz/integer-overflow-or-underflow/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,22 +1,20 @@
 #![no_main]
+#![feature(core_panic)]
 
 use libfuzzer_sys::fuzz_target;
-use integer_overflow_or_underflow::integer_overflow_underflow;
+use integer_overflow_or_underflow::integer_overflow_underflow::IntegerOverflowUnderflow;
 use arbitrary::Arbitrary;
 
 #[derive(Clone, Debug, Arbitrary)]
 #[cfg_attr(feature = "arbitrary", derive(arbitrary::Arbitrary))]
 pub struct Input {
-    pub valueForNew: u8,
-    pub valueForAdd: u8,
-    pub valueForSub: u8,
+
 }
 
 fuzz_target!(|input: Input| {
-    let mut contract = integer_overflow_underflow::IntegerOverflowUnderflow::new(input.valueForNew);
-    contract.add(input.valueForAdd);
-    contract.sub(input.valueForSub);
-    contract.get();
+
+    // do nothing
+
 });
 
 
